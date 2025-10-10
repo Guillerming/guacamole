3255110f350320f0f0b3d4e35f2fbab1877c6ad6
Guillerming <guillerming@gmail.com>
2025-10-10 23:14:30 +0100

    AppConfig::baseUrl

--- src/Guacamole/Config/AppConfig.php
+++ src/Guacamole/Config/AppConfig.php
@@ -4,7 +4,20 @@ declare(strict_types=1);
 
 namespace Guacamole\Config;
 
+use Guacamole\Helpers\StringHelper;
+
 class AppConfig {
+    static function baseUrl(?string $append = null): string {
+        $httpsPort = Env::get('HTTPS_PORT');
+        assert(gettype($httpsPort) == 'string');
+        $baseUrl = 'https://localhost:'.$httpsPort;
+        if ($append) {
+            $baseUrl .= $append;
+        }
+
+        return StringHelper::mergeSlashes($baseUrl);
+    }
+
     const GuacamoleVersion = '0.0.0-rc1';
     const ProjectVersion = '0.0.0';
 }
