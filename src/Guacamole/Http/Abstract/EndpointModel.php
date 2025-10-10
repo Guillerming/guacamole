cb5a8bd6521fdfb3998a2aa5870fbc9030db00fa
Guillerming <guillerming@gmail.com>
2025-10-10 23:15:46 +0100

    Add default ContentType for endpoints

--- src/Guacamole/Http/Abstract/EndpointModel.php
+++ src/Guacamole/Http/Abstract/EndpointModel.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Guacamole\Http\Abstract;
 
+use Guacamole\Helpers\HeaderSupport\Enum\Header;
 use Guacamole\Http\Response;
 
 abstract class EndpointModel extends HttpResource {
+    public function __construct() {
+        self::addHeader(Header::ContentTypeApplicationJson);
+    }
+
     /**
      * This method is called to get the response of the endpoint.
      * */
